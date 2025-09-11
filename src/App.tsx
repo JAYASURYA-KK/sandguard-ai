e0ae0fadbed0216e44ebe1f177236966914093f7
JAYASURYA K <jayasurya272007@gmail.com>
2025-09-11 15:08:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index.txt";
-import NotFound from "./pages/NotFound.txt";
-import PredictPage from "./pages/PredictPage.txt";
+import Index from "./pages/Index.tsx";
+import NotFound from "./pages/NotFound.tsx";
+import PredictPage from "./pages/PredictPage.tsx";
 
 const queryClient = new QueryClient();
 
